db2dcedfd938043a904bc8a3c53a85c8f44fb4c1
Kelvin Fichter <kelvinfichter@gmail.com>
2018-12-20 14:29:33 -0500

    Add eslint

--- src/merkle-sum.js
+++ src/merkle-sum.js
@@ -1,53 +1,53 @@
-const web3 = require('web3');
+const web3 = require('web3')
 
 class Node {
-  constructor(data, sum) {
-    this.data = data;
-    this.sum = sum;
+  constructor (data, sum) {
+    this.data = data
+    this.sum = sum
   }
 }
 
 class MerkleSum {
-  constructor(leaves) {
+  constructor (leaves) {
     if (!leaves) {
-      leaves = [];
+      leaves = []
     }
 
     leaves = leaves.map((leaf) => {
-      return new Node(this.hash(leaf.data), leaf.sum);
-    });
+      return new Node(this.hash(leaf.data), leaf.sum)
+    })
 
-    this.levels = this.generate(leaves, [leaves]);
+    this.levels = this.generate(leaves, [leaves])
   }
 
-  root() {
-    return this.levels[this.levels.length - 1][0];
+  root () {
+    return this.levels[this.levels.length - 1][0]
   }
 
-  hash(value) {
-    return web3.utils.sha3(value).slice(2);
+  hash (value) {
+    return web3.utils.sha3(value).slice(2)
   }
 
-  parent(left, right) {
-    return new Node(this.hash(left.data + right.data), (left.sum + right.sum));
+  parent (left, right) {
+    return new Node(this.hash(left.data + right.data), (left.sum + right.sum))
   }
 
-  generate(children, levels) {
+  generate (children, levels) {
     if (children.length <= 1) {
-      return [children];
+      return [children]
     }
 
-    let parents = [];
+    let parents = []
     for (let i = 0; i < children.length; i += 2) {
-      let left = children[i];
-      let right = (i + 1 == children.length) ? new Node(0, 0) : children[i + 1];
-      parents.push(this.parent(left, right));
+      let left = children[i]
+      let right = (i + 1 === children.length) ? new Node(0, 0) : children[i + 1]
+      parents.push(this.parent(left, right))
     }
 
-    levels.push(parents);
-    this.generate(parents, levels);
-    return levels;
+    levels.push(parents)
+    this.generate(parents, levels)
+    return levels
   }
 }
 
-module.exports = MerkleSum;
+module.exports = MerkleSum
