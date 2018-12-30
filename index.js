1f0fc4ac4da2d810819198aaf05c350dc895b22f
Karl Floersch <karl@karlfloersch.com>
2018-12-30 17:10:50 -0500

    Add encoder and sumTree to exports

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const utils = require('./src/utils')
+const encoder = require('./src/transaction-serialization.js')
+const sumTree = require('./src/sum-tree/plasma-sum-tree.js')
 const logging = require('./src/logging')
 
 module.exports = {
   utils,
-  logging: logging
+  sumTree,
+  logging,
+  encoder
 }
