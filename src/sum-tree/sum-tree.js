b1032aa3d5c5b12d0f6bb6102d8024c294f32ba5
Benjamin Jones <ben@Benjamins-MacBook-Pro.local>
2018-12-30 11:26:12 -0500

    fix some formatting

--- src/sum-tree/sum-tree.js
+++ src/sum-tree/sum-tree.js
@@ -28,7 +28,6 @@ class MerkleSumTree {
   }
 
   emptyLeaf () {
-    debugger
     return new MerkleTreeNode('0x0000000000000000000000000000000000000000000000000000000000000000', 0)
   }
 
@@ -46,7 +45,6 @@ class MerkleSumTree {
       let left = children[i]
       let right = (i + 1 === children.length) ? this.emptyLeaf() : children[i + 1]
       let parent = this.parent(left, right)
-      debugger
       parents.push(parent)
     }
 
