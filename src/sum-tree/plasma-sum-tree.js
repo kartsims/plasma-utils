32427af7b9ec75fe132bc6c79cf3219d0de9037d
Benjamin Jones <ben@Benjamins-MacBook-Pro.local>
2018-12-29 19:38:22 -0500

    change 'end' to final coinID + 1

--- src/sum-tree/plasma-sum-tree.js
+++ src/sum-tree/plasma-sum-tree.js
@@ -4,33 +4,37 @@ const BN = require('web3').utils.BN
 
 class PlasmaMerkleSumTree extends MerkleSumTree {
   parseLeaves (leaves) {
+    leaves = leaves.map((leaf) => {
+      let enc = leaf.encode()
+      let TR = leaf.transferRecords.elements[leaf.TRIndex]
+      return {
+        data: this.hash('0x' + new BN(enc).toString(16, 2 * enc.length)),
+        typedStart: TR.type.toString(16, 8) + TR.start.toString(16, 24),
+        typedEnd: TR.type.toString(16, 8) + TR.end.toString(16, 24)
+      }
+    })
+    leaves[0].start = 0 // start of the leaf's coverage is 0 to the sum tree, even if TR is not
+    leaves.push({typedStart: new BN('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',16)}) // add a fake final TR which happens at the final coinpost
     for (let i = 0; i < leaves.length - 1; i++) {
-      let leaf = leaves[i]
-      let nextLeaf = leaves[i+1]
-      let enc = leaf.encode();
-      leaves[i].TXData = '0x' + new BN(enc).toString(16, 2 * enc.length)
-      let thisTR = leaf.transferRecords.elements[leaf.TRIndex]
-      let nextTR = nextLeaf.transferRecords.elements[nextLeaf.TRIndex]
-      let typeDiff = nextTR.type.sub(thisTR.type)
-      let rangeDiff = (i === 0) ? nextTR.start : nextTR.start.sub(thisTR.start) // first one always starts at 0 in terms of proofs even if the first TR start isn't 0
-      let coverage = typeDiff.toString(16, 8) + rangeDiff.toString(16, 24)
-      leaves[i].range =  new BN(coverage, 16)// covers from the start of this tx to the start of the next, making an implicit notx in between
-    }
-    if (leaves.length === 1) { // weird edge case with only 1 TX in block --> covers 0 to end --> range = end+1
-      let typeDiff = leaves[0].transferRecords.elements[0].type
-      let rangeDiff = leaves[0].transferRecords.elements[0].end.add(new BN(1))
-      debugger
-      let coverage = typeDiff.toString(16, 8) + rangeDiff.toString(16, 24)
-      leaves[0].range = new BN(coverage, 16)
-    } else { // otherwise there's more than one tx and we set the last range as follows
-      let lastLeaf = leaves[leaves.length-1] // set encoding data of last leaf since we missed it above
-      let enc = lastLeaf.encode();
-      leaves[leaves.length-1].TXData = '0x' + new BN(enc).toString(16, 2 * enc.length)  
-      let lastTR = lastLeaf.transferRecords.elements[lastLeaf.TRIndex]
-      leaves[leaves.length-1].range = lastTR.end.sub(lastTR.start).add(new BN(1))
+      leaves[i].range = leaves[i+1].typedStart.sub(leaves[i].typedStart)
     }
     return leaves.map((leaf) => {
-      return new MerkleTreeNode(this.hash(leaf.TXData), leaf.range)
+      return new MerkleTreeNode(leaf.data, leaf.range)
+    })
+  }
+
+  getBranch(leafIndex) {
+    if (leafIndex >= this.levels[0].length || leafIndex < 0) {throw new Error('invalid branch index requested')}
+    // let path = new BN(leafIndex).toString(2, this.levels.length-1)
+    let proof = []
+    let nodeIndex = Math.floor((leafIndex + 1) / 2) * 2
+    for (let i = 0; i < this.levels.length; i++) {
+      proof.push(this.levels[i][nodeIndex])
+      proof.push(this.levels[i][nodeIndex+1])
+      nodeIndex = Math.floor(nodeIndex / 2)
+    }
+    return proof.map((node) => {
+      return (node === undefined) ? this.emptyLeaf() : node
     })
   }
 }
